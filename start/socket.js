166b57d482fb393ba0663add2abe8122fa757dd3
Your Name <you@example.com>
2021-04-27 16:54:51 -0500

    socket.io starting point

--- start/socket.js
+++ start/socket.js
@@ -12,7 +12,21 @@
 | For middleware, do check `wsKernel.js` file.
 |
 */
+const  express  =  require ( 'express' ) ;
+const  socketIO  =  require ( 'socket.io' ) ;
 
-const Ws = use('Ws://invernaderoeq5.herokuapp.com')
+const  PORT  =  proceso . env . PUERTO  ||  3000 ;
+const  INDEX  =  '/index.html' ;
 
-Ws.channel('chat', 'ChatController')
+ const servidor =  express ( )
+  . use ( ( req ,  res )  =>  res . sendFile ( INDEX ,  {  root : __dirname  } ) )
+  . listen ( PORT ,  ( )  =>  console . log ( `Escuchando en $ { PORT } ` ) ) ;
+
+const  io  =  socketIO ( servidor ) ;
+
+io . on ( 'conexión' ,  ( enchufe )  =>  {
+  consola . log ( 'Cliente conectado' ) ;
+  zócalo . on ( 'desconectar' ,  ( )  =>  consola . log ( 'Cliente desconectado' ) ) ;
+} ) ;
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
